a80a21755b393a86b6542adce67e9c2119901cc7
estk <esims89@gmail.com>
2011-05-20 18:39:52 -0700

    stuffed into classes

--- autogg.rb
+++ autogg.rb
@@ -6,73 +6,65 @@
 
 
 require 'optparse'
+require 'ostruct'
 require 'find'
 #require 'rb-inotify' only when --watch is passed
 
 # Comandline argument parsing function ----
+class Parser
+  def self.parse(args)
 
-def parseargs
-  options = {}
-  dirinfo = "Please use absolute paths"
+    script_name = File.basename( $0 )
+    dirinfo = "Please use absolute paths"
+    options = OpenStruct.new
+    options.watch = false
+    options.oggargs = []
 
-  op = OptionParser.new do |opts|
-    opts.banner = "Usage: autogg.rb [options] flacpath oggpath [ -o oggenc args ... ]"
+    op = OptionParser.new do |opts|
+      opts.banner = "Usage: autogg.rb [options] flacpath oggpath [ -o oggenc args ... ]\n" +
+                    "Example: #{script_name} ~/music/flac/ ~music/ogg/ --watch --oggenc-args BLAH"
 
-    options[:watch] = false
-    opts.on( '-w', '--watch', 
-            "Watch flacpath for changes, then rescan " +
-            "and encode if any files are created within" ) do
-      options[:watch] = true
+      opts.on( '-w', '--watch',
+              "Watch flacpath for changes, then rescan " +
+              "and encode if any files are created within" ) do
+        options.watch = true
+      end
+
+      opts.on( '-m', '--max-processes n', "Maximum number of encoding processes running at once" ) do |n|
+        options.max_procs = n
+      end
+
+      opts.on( '-o', '--oggenc-args arglist', Array,
+               "Specify arguments to me be passed through to oggenc" ) do |ary|
+        options.oggargs = ary
+      end
+
+      opts.on( '-h', '--help', 'Display this screen' ) do
+        puts opts
+        puts dirinfo
+      end
     end
 
-    options[:oggargs] = nil
-    opts.on( '-o', '--oggenc-args', 
-             "Specify arguments to me be passed " +
-             "through to each oggenc" ) do
-      options[:oggargs] = true
+    op.parse!
+
+    if ( ARGV.length = 2 ) and ARGV.all? { |a| File.directory?( a )}
+      paths = OpenStruct.new
+      paths.flac = ARGV[0] ; paths.ogg = ARGV[1]
+      options.paths = paths
+    else
+      puts op
+      exit
     end
 
-    opts.on( '-h', '--help', 'Display this screen' ) do
-      puts opts
-      puts dirinfo
-    end
-  end
-
-  op.parse!
-
-  if ( ARGV.length >= 2 ) and ( ARGV[0..1].all? { |a| a =~ /\/.*\// } )
-    @flacpath, @oggpath = ARGV[0..1]
-  else
-    puts op
-    exit
-  end
-
-  if options[:oggargs]       ## maybe there is a beter way to parse oggargs?
-    @oggargs = ARGV[2..-1]
-  else
-    @oggargs = []
-  end
-end
-
-# directory traversal ------ DONE
-
-def oggencdir
-  @ps_hash = SizedPsHash.new(4)
-  Find.find( @flacpath ) do |path|
-    if FileTest.directory?( path )
-      Find.prune if File.basename( path )[0] == ?.
-    elsif File.flac?( path )
-      encfile( path )
-    end
+    options
   end
 end
-
 
 # class definitions ----------------- DONE
 
-class File
-  def self.flac?(path)
-    self.file?(path) and self.basename(path) =~ /\.flac/
+class Flac < File
+  def self.exists?(path)
+    self.file?( path ) and self.basename( path ) =~ /\.flac/
   end
 end
 
@@ -82,7 +74,7 @@ class SizedPsHash < Hash
   ## Blocks until (its hash) size less than @max to add a new process.
   ## Automatically removes completed processes.
 
-  def self.initialize(max)
+  def self.initialize( max )
     @max = max
     super
   end
@@ -96,43 +88,64 @@ class SizedPsHash < Hash
   end
 end
 
-# utilities -----------------
+class OggEncoder
 
-def encfile( indir )
-  outdir = indir.gsub( @flacpath, @oggpath )
-  ps = IO.popen %Q{oggenc #{@oggargs.join} "#{indir}" -o "#{outdir}"}
-  @ps_hash[ps.pid] = outdir
-end
+  class << self
 
-def interupt
-  puts "\n" + "Shutting down and removing partially encoded files in #{@cwd}"
-  @ps_hash.each do |pid, path|
-    File.delete( path )
+    def oggencdir
+      Find.find( @paths.flac ) do |path|
+        if FileTest.directory?( path )
+          Find.prune if File.basename( path )[0] == ?.
+        elsif Flac.exists?( path )
+          self.encfile( @paths, path )
+        end
+      end
+    end
+
+    def encfile( indir )
+      outdir = indir.gsub( @paths.flac, @paths.ogg )
+      ps = IO.popen %Q{oggenc #{@oggargs.join} "#{indir}" -o "#{outdir}"}
+      @ps_hash[ps.pid] = outdir
+    end
+
+    def interupt
+      puts "\n" + "Shutting down and removing partially encoded files in #{@cwd}"
+      @ps_hash.each do |pid, path|
+        File.delete( path )
+      end
+    end
+
+    def watcher
+      ##INCOMPLETE
+      ## needs separate interupt signal handling
+      notifier = INotify::Notifier.new
+      notifier.watch( @flacpath, :create ) do |e|
+        puts e.name + " was modified, rescaning..."
+        self.encode
+      end
+
+      puts "watching #{@flacpath}"
+      notifier.run
+    end
+
+    def encode( options )
+      @options = options
+      @paths = options.paths
+      @oggargs = options.oggargs
+      @ps_hash = SizedPsHash.new( option.max_procs )
+      self.oggencdir ; Process.waitall
+      watcher if options.watch
+    end
   end
-end
 
-def watcher
-  ##INCOMPLETE
-  ## needs separate interupt signal handling
-  notifier = INotify::Notifier.new
-  notifier.watch( @flacpath, :create ) do |e|
-    puts e.name + " was modified, rescaning..."
-    oggencdir ; Process.waitall ; watcher
+  trap "INT" do
+    interupt
   end
-
-  puts "watching #{@flacpath}"
-  notifier.run
-end
-
-# run the code -------------
-
-trap "INT" do
- interupt
 end
 
 if __FILE__ == $0
-  parseargs ; oggencdir ; Process.waitall
-  watcher if options[:watch]
+  options = Parser.parse
+  OggEncoder.encode( options )
 end
 
 ## TODO
